0eb7d910c86d9074d5bdc2f5ac859bc1a6a52255
Dongha Yi <ceokingmaker@naver.com>
2022-05-09 23:52:15 +0900

    Part1_17_Contact_Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_service/contacts_service.dart';
 // permission handler 추가 완료
 // 패키지 설치와 더불어 gradle파일과 Manifest파일 세팅들이 끝나면 emul재시작 필요
+// 연락처를 활용할 수 있도록 하는 패키지 import함
 
 //void는 '아무 것도 하지 말아주세요' 인데 알 필요 없음
 //void main() {} 아래 다 지우고 새로 작성
@@ -36,6 +38,25 @@ class _MyAppState extends State<MyApp> {
     var status = await Permission.contacts.status;
     if (status.isGranted) {
       print('허락됨');
+      var contacts = await ContactsService.getContacts();
+    //  이렇게 한 줄 쓰면 허락됐을 때 연락처를 가져오게 됨
+    //  하지만 초기에 연락처가 없을 것이기에 연락처를 몇 개 만듦
+    //  withThumbnails: false라고 파라미터(?)안에 넣어주면 썸네일 없이 빠르게 가져옴
+      print(contacts[0].displayName);
+    //  contacts는 instance of 'Contacts'로 print 됨
+    //  따라서 contacts[0] 쓰고 .(점)찍고 안에 정보를 뺌
+    //   var newPerson = Contact();
+    //  연락처 정보를 newPerson에 넣고 새사람과 관련된 정보 추가
+    //  커스텀 위젯을 쓸 때는 new Contact();처럼 new를 붙여줘야하는데... 생략 가능
+      setState(() {
+        // setState 함수 안에 넣어서 실시간으로 데이터 변동 감지
+        name = contacts;
+      });
+      // newPerson.givenName = '민수';
+      // newPerson.familyName = '김';
+      // await ContactsService.addContact(newPerson);
+    //  위처럼 쓰면 '민수 김' 정보를 실제 연락처에 저장할 수 있음
+
     } else if (status.isDenied) {
       print('거절됨');
       Permission.contacts.request();
@@ -49,6 +70,24 @@ class _MyAppState extends State<MyApp> {
   // 따라서 실제 어떤 권한이 필요한 순간에 권한을 요청하는 것이 필요함
   // async await는 자바스크립트의 promise랑 유사, but Futures라고 부름
 
+  addPerson (given,family,number) async {
+    var newPerson = Contact();
+    //newPerson은 Contact란 녀석이다!로 정의
+    newPerson.givenName = given;
+    newPerson.familyName = family;
+    newPerson.phones = [Item(label: "mobile", value: number)];
+    //캬, 나 스스로 발견한 Contact패키지의 .phones 사용법!
+    await ContactsService.addContact(newPerson);
+  //  연락처에 이름 때려박아 줌
+    var contacts = await ContactsService.getContacts();
+  //  추가된 연락처 다시 불러와주고
+    setState(() {
+      name = contacts;
+    });
+  //  새롭게 name안에 연락처 넣어줌
+  }
+
+
   // @override
   // void initState() {
   //   // TODO: implement initState
@@ -59,7 +98,8 @@ class _MyAppState extends State<MyApp> {
   // }
   // 화면 실행시 보여지는 함수, init만 입력하면 자동완성으로 initState가 뜸
 
-  var name = ['전혁', '송용수', '김동욱',];
+  var name = [];
+  // name 안에 요소를 넣게 되면 바로 name의 형식이 정해지기에 빈 칸으로 수정
   var a = 0;
   var total = 0;
 addOne(){
@@ -69,12 +109,12 @@ addOne(){
 }
   //이처럼 함수를 부모 class 밑에 '설치 > 전달 > 받고 > 정의 > 적용'
 
-  addName(txt) {
-  setState(() {
-    //하, setState 자꾸 까먹지 말자 ㅠㅠ 몇번째..?
-    name.add(txt);
-  });
-  }
+  // addName(txt) {
+  // setState(() {
+  //   //하, setState 자꾸 까먹지 말자 ㅠㅠ 몇번째..?
+  //   name.add(txt);
+  // });
+  // }
   
   
   @override
@@ -89,14 +129,14 @@ addOne(){
     // return Container(
     return Scaffold(
           floatingActionButton: FloatingActionButton(
-            child: Text(a.toString()),
+            child: Icon(Icons.add),
             //버튼을 누르면 숫자가 올라가도록 코드 작성
             onPressed: (){
               showDialog(context: context, builder: (context){
                 //showDialog에는 context와 builder, return이 필요함
                 //하지만 context는 위젯의 족보로 위에 있는 위젯을 보여줌
                 return Dialog(
-                  child: DialogDesign(naming: a, addOne: addOne, addName: addName, name: name),
+                  child: DialogDesign(naming: a, addOne: addOne, name: name, addPerson : addPerson),
                 //  부모의 데이터를 자식 Widget에서 쓰기 위해 넣어줌
                 );
               });
@@ -148,7 +188,10 @@ class _ProfileListState extends State<ProfileList> {
         return ListTile(
 //           //슨생님은 ListTile을 이용함
           leading: Icon(Icons.account_circle, size: 40, color: Colors.black),
-          title: Text(widget.name[i]),
+          title: Text(widget.name[i].givenName),
+          // name[i]를 그대로 두면 String이 아니라는 Error가 남,
+          // 처음에 .FirstName이라고도 했는데 instance of 'Contracts'에 없는 규칙이었음
+          // 패키지(라이브러리?) 사용법 확인이 필요한 부분
 //
 //         //  int는 Text에 안 나와서 .toString() 붙여야 함
         );
@@ -158,7 +201,7 @@ class _ProfileListState extends State<ProfileList> {
 }
 
 class DialogDesign extends StatefulWidget {
-  DialogDesign({Key? key, this.naming, this.addOne, this.addName, this.name}) : super(key: key);
+  DialogDesign({Key? key, this.naming, this.addOne, this.name, this.addPerson}) : super(key: key);
   // 기존 Key? key 부분은 꼭 남겨두자. this.naming을 통해 부모에게서 정보 가져옴
   // 중괄호 부분에 parameter를 설정할 수 있음(중괄호는 optional하게 두 개를 받는다는 의미? 하나만 넣어도 됨?)
   // naming이란 parameter를 쓰겠습니다는 얘기, but 아래에 변수를 설정해둬야 함
@@ -167,8 +210,11 @@ class DialogDesign extends StatefulWidget {
   var addOne;
   var inputData = TextEditingController();
   //TextEditingController() 안에 Input 데이터가 들어감
+  var familyName = TextEditingController();
+  var phoneNumber = TextEditingController();
   var addName;
   var name;
+  var addPerson;
 
   @override
   State<DialogDesign> createState() => _DialogDesignState();
@@ -182,22 +228,44 @@ class _DialogDesignState extends State<DialogDesign> {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(32, 40, 32, 24),
-      height: 280,
+      height: 320,
       width: 400,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Flexible(child: Text('Contact', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)), flex: 1, fit: FlexFit.tight,),
+          Flexible(child: Text('Contact', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)), flex: 1, fit: FlexFit.tight,),
           Flexible(child: TextField(
             controller: widget.inputData,
+            // controller가 같으면 같이 입력됨
+            // 아래 TextField에도 .inputData면 같은 내용으로 글이 써짐
             //위치에 따라 widget.붙여야 하는 것 잊지말자!
             //controller 대신 onChanged: (text) { widget.inputData = text }, 사용 가능
             //onChange는 inputData가 많을 경우 사용
             //하지만 우리는 list[]나 Map{}자료를 사용할 예정
             decoration: InputDecoration(
               border: OutlineInputBorder(),
-              labelText: 'name',
+              labelText: 'First Name',
+            ),
+          ), flex: 1, fit: FlexFit.tight,),
+          Flexible(child: SizedBox(
+            height: 10,
+          )),
+          Flexible(child: TextField(
+            controller: widget.familyName,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Family Name',
+            ),
+          ), flex: 1, fit: FlexFit.tight,),
+          Flexible(child: SizedBox(
+            height: 10,
+          )),
+          Flexible(child: TextField(
+            controller: widget.phoneNumber,
+               decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Phone Number',
             ),
           ), flex: 1, fit: FlexFit.tight,),
           Flexible(
@@ -210,10 +278,14 @@ class _DialogDesignState extends State<DialogDesign> {
               //  '현재 페이지 닫아주셈'과 같은 의미, 나중에 다시 나옴
               }, child: Text('Cancel', style: TextStyle(fontSize: 18),)),
               TextButton(onPressed: () {
-                  widget.addName(widget.inputData.text.toString());
+                  // widget.addName(widget.inputData.text.toString());
               //    inputData List 안, text를 toString해서 보여주자!
+                  print(widget.inputData.text.toString());
+                  widget.addPerson(widget.inputData.text,
+                      widget.familyName.text,
+                      widget.phoneNumber.text,
+                  );
                   Navigator.pop(context);
-                  print(widget.name);
 
               }, child: Text('OK', style: TextStyle(fontSize: 18),)),
             ],
